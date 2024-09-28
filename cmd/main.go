81a709bba4b5316edaf050afa0ec0b8f150ffa91
Lucas Peixer <Lucaspeicher@hotmail.com>
2024-09-28 00:22:19 -0300

    feat: aplica todas as camadas de usecase,repository, model, controller e DB

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"go-api/controller"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/usecase"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	conn, err := db.connectDB()
+
+	dbConnection, err := db.ConnectDB()
+
 	if(err != nil){
 		panic(err)
 	}
-
+	bookRepository := repository.NewBookRepository(dbConnection)
+	//Camada UseCase
+	bookUseCase := usecase.NewBookUseCase(bookRepository)
+	//Camada de Controllers
+	bookController := controller.NewBookController(bookUseCase)
+	
 	server := gin.Default()
 
 	server.GET("/ping", func(ctx *gin.Context) {
@@ -18,6 +30,8 @@ func main() {
 				"message": "pong",
 		})
 	})
+
+	server.GET("/book", bookController.GetBooks)
 
 	server.Run(":8000")
 }
