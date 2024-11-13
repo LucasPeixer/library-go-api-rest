449e2037ee49e8a0efdf35a49ab3cffe49b659e4
PoweredTable <poweredlucas@gmail.com>
2024-11-13 20:36:13 -0300

    feat: implement godotenv

--- initializers/load_env.go
+++ initializers/load_env.go
@@ -3,6 +3,8 @@ package initializers
 import (
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 // DbDSN é a DSN (data source name) para conexão com banco de dados.
@@ -13,6 +15,14 @@ var JwtKey []byte
 
 // LoadEnv carrega as variáveis de ambiente necessárias.
 func LoadEnv() {
+	// Carrega as variáveis do arquivo .env se existir
+	if _, err := os.Stat(".env"); err == nil {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
 	DbDSN = os.Getenv("DB_DSN")
 	if DbDSN == "" {
 		log.Fatal("DB_DSN environment variable not set")
