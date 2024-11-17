b54ca6f77fb33231229cf8dfff8b99acc164f540
Waldyr Rodrigues da Costa <waldyr.programing@gmail.com>
2024-11-17 19:47:14 -0300

    feat: add routes to the swagger

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,9 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/gin-swagger"
+    "github.com/swaggo/files"  // Pacote correto para servir os arquivos Swagger
+    _ "go-api/docs" // Atualize conforme o nome do módulo no go.mod
 )
 
 // Routes registra todas as rotas http.
@@ -9,4 +12,7 @@ func Routes(r *gin.Engine) {
 	api := r.Group("/api/v1")
 	UserRoutes(api)
 	BookRoutes(api)
+	
+	api.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, ginSwagger.URL("/api/v1/swagger.json")))
+	api.StaticFile("/swagger.json", "./docs/swagger.json")
 }
