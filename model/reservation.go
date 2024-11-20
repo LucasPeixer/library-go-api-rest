e11dfc6057bf098c96373407b785860926820ef8
lucas Peixer <lucaspeicher@hotmail.com>
2024-11-19 21:03:36 -0300

    feat: adiciona o tipo ReservationStatus no model

--- model/reservation.go
+++ model/reservation.go
@@ -2,13 +2,23 @@ package model
 
 import "time"
 
+type ReservationStatus string
+
+const (
+	Pending  ReservationStatus = "pending"
+	Cancelled ReservationStatus = "cancelled"
+	Expired   ReservationStatus = "expired"
+	Collected ReservationStatus = "collected"
+	Finished ReservationStatus = "finished"
+)
+
 type Reservation struct {
-	ID           string    `json:"id" db:"id"`
+	ID           int    `json:"id" db:"id"`
 	ReservedAt   time.Time `json:"reserved_at" db:"reserved_at"`
 	ExpiresAt    time.Time `json:"expires_at" db:"expires_at"`
 	BorrowedDays int       `json:"borrowed_days" db:"borrowed_days"`
-	Status       string    `json:"status" db:"status"`
-	UserID       string    `json:"fk_user_id" db:"fk_user_id"`
-	AdminID      string    `json:"fk_admin_id" db:"fk_admin_id"`
-	BookID       string    `json:"fk_book_id" db:"fk_book_id"`
+	Status       ReservationStatus    `json:"status" db:"status"`
+	UserID       int    `json:"fk_user_id" db:"fk_user_id"`
+	AdminID      int    `json:"fk_admin_id" db:"fk_admin_id"`
+	BookID       int    `json:"fk_book_id" db:"fk_book_id"`
 }
