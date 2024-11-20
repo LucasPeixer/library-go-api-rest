962c265bfda937813ea6459825f831310dcc2cae
lucas Peixer <lucaspeicher@hotmail.com>
2024-11-19 22:23:33 -0300

    adiciona a regra de CreateReservation no controller

--- controller/reservation_controller.go
+++ controller/reservation_controller.go
@@ -28,3 +28,47 @@ func (rc *ReservationController) GetReservationsByFilters(c *gin.Context) {
 
 	c.JSON(http.StatusOK, reservations)
 }
+
+func (rc *ReservationController) CreateReservation(c *gin.Context) {
+
+	userIDStr, exists := c.Get("user_id") 
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized user"})
+		return
+	}
+
+	userID, err := strconv.Atoi(userIDStr.(string))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	// Pegando os dados da reserva do corpo da requisição
+	var request model.ReservationRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid data"})
+		return
+	}
+
+	// Atribuindo o ID do usuário na reserva
+	request.UserID = userID
+
+	// Criando a reserva
+	reservation, err := rc.UseCase.CreateReservation(&request)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("error when creating reservation: %v", err)})
+		return
+	}
+
+	// Retornando o sucesso com a reserva criada
+	c.JSON(http.StatusCreated, gin.H{
+		"id":          reservation.ID,
+		"reserved_at": reservation.ReservedAt,
+		"expires_at":  reservation.ExpiresAt,
+		"status":      reservation.Status,
+		"borrowed_days": reservation.BorrowedDays,
+		"fk_user_id":   reservation.UserID,
+		"fk_book_id":   reservation.BookID,
+	})
+}
+
