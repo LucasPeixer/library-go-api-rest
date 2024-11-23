7af745022fe8c5a4ad06b81b2a2b3a7915065afb
lucas Peixer <lucaspeicher@hotmail.com>
2024-11-22 22:09:50 -0300

    feat: adiciona a função para buscar as reservas do usuario pelo ID

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -15,6 +15,7 @@ type UserRepository interface {
 	GetUsersByFilters(name, email string) (*[]user.Account, error)
 	GetUserById(id int) (*user.Account, error)
 	GetUserLoans(userID int) ([]model.Loan, error)
+	GetUserReservation(userID int) ([]*model.Reservation, error)
 	ActivateUser(id int) error
 	DeactivateUser(id int) error
 	DeleteUser(id int) error
@@ -207,6 +208,46 @@ func (ur *userRepository) GetUserLoans(userID int) ([]model.Loan, error){
 	}
 
 	return loans, nil
+}
+
+func (ur *userRepository) GetUserReservation(userID int) ([]*model.Reservation, error) {
+	query := `
+		SELECT 
+			r.id, 
+			r.fk_user_id, 
+			r.fk_book_id, 
+			r.borrowed_days, 
+			r.status, 
+			r.reserved_at, 
+			r.expires_at
+		FROM reservation r
+		WHERE r.fk_user_id = $1
+	`
+	rows, err := ur.DB.Query(query, userID)
+	if err != nil {
+		log.Printf("Error executing query: %v", err)
+		return nil, fmt.Errorf("error fetching reservations: %w", err)
+	}
+	defer rows.Close()
+
+	var reservations []*model.Reservation
+
+	for rows.Next() {
+		var reservation model.Reservation
+		if err := rows.Scan(&reservation.ID, &reservation.FkUserId, &reservation.FkBookId, &reservation.BorrowedDays, &reservation.Status, &reservation.ReservedAt, &reservation.ExpiresAt); err != nil {
+			log.Printf("Error reading reservation data: %v", err)
+			return nil, fmt.Errorf("error reading reservation data: %w", err)
+		}
+
+		reservations = append(reservations, &reservation)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Printf("Error iterating over reservations: %v", err)
+		return nil, fmt.Errorf("error iterating over reservations: %w", err)
+	}
+
+	return reservations, nil
 }
 
 func (ur *userRepository) ActivateUser(id int) error {
