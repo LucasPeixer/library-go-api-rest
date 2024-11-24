9fef2f799537ddeca4f05f55dce93e13c0c4a368
PoweredTable <poweredlucas@gmail.com>
2024-11-24 00:21:24 -0300

    feat: add reservation cancel routes and reservation get

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"errors"
+	"fmt"
 	"go-api/model"
 	"go-api/model/user"
 	"go-api/repository"
@@ -17,6 +18,8 @@ type UserUseCase interface {
 	ActivateUser(id int) error
 	DeactivateUser(id int) error
 	DeleteUser(id int) error
+	GetUserReservations(id int) ([]*model.Reservation, error)
+	CancelUserReservation(id, reservationId int, adminId *int) error
 }
 
 type userUseCase struct {
@@ -72,4 +75,17 @@ func (uu *userUseCase) DeactivateUser(id int) error {
 
 func (uu *userUseCase) DeleteUser(id int) error {
 	return uu.userRepo.DeleteUser(id)
+}
+
+func (uu *userUseCase) CancelUserReservation(id, reservationId int, adminId *int) error {
+	res, err := uu.userRepo.GetUserReservationById(id, reservationId)
+	if err != nil {
+		return err
+	}
+
+	if res.Status != model.Pending {
+		return fmt.Errorf("cannot cancel user reservation unless its status is 'pending'. Current status: '%s'", res.Status)
+	}
+
+	return uu.userRepo.CancelUserReservation(id, reservationId, adminId)
 }
