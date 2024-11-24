e104baf0cb394b11ca4cceee7ac17dbde4d79c1d
lucas Peixer <lucaspeicher@hotmail.com>
2024-11-23 22:12:11 -0300

    chora: ajusta a query e o processo de scan do repository

--- repository/loan_repository.go
+++ repository/loan_repository.go
@@ -19,18 +19,31 @@ func NewLoanRepository(db *sql.DB) LoanRepository {
 	return &loanRepository{db: db}
 }
 
-func (lr *LoanRepository) CreateLoan(loanRequest *model.LoanRequest) (*model.Loan, error) {
+func (lr *LoanRepository) CreateLoan(loan *model.Loan) (*model.Loan, error) {
 	query := `
 			INSERT INTO loan (return_by, fk_book_stock_id, fk_reservation_id)
 			VALUES ($1, $2, $3)
 			RETURNING id, loaned_at, return_by, returned_at, status, fk_admin_id, fk_book_stock_id, fk_reservation_id`
-	
-	var loan model.Loan
-	err := lr.db.QueryRow(query, loanRequest.ReturnBy, loanRequest.AdminID, loanRequest.BookStockID, loanRequest.ReservationID).
-			Scan(&loan.ID, &loan.LoanedAt, &loan.ReturnBy, &loan.ReturnedAt, &loan.Status, &loan.AdminID, &loan.BookStockID, &loan.ReservationID)
+
+	var createdLoan model.Loan
+	err := lr.db.QueryRow(
+			query,
+			loan.ReturnBy,
+			loan.BookStockID,
+			loan.ReservationID,
+	).Scan(
+			&createdLoan.ID,
+			&createdLoan.LoanedAt,
+			&createdLoan.ReturnBy,
+			&createdLoan.ReturnedAt,
+			&createdLoan.Status,
+			&createdLoan.AdminID,
+			&createdLoan.BookStockID,
+			&createdLoan.ReservationID,
+	)
 	if err != nil {
 			return nil, fmt.Errorf("error inserting loan: %w", err)
 	}
 
-	return &loan, nil
+	return &createdLoan, nil
 }
