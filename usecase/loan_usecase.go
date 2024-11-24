62931fc4090110bd29a0cd4b8f39f537461e1a2e
lucas Peixer <lucaspeicher@hotmail.com>
2024-11-24 16:54:55 -0300

    chore: reajusta a estrutura de ação de criação de emprestimo no usecase

--- usecase/loan_usecase.go
+++ usecase/loan_usecase.go
@@ -25,42 +25,41 @@ func NewLoanUseCase(loanRepo repository.LoanRepositoryInterface, reservationRepo
 
 func (lu *LoanUseCase) CreateLoanAndUpdateReservation(request *model.LoanRequest) (*model.Loan, error) {
 
-	/*reservation, err := lu.reservationRepo.GetReservationByID(request.ReservationID)
+	reservation, err := lu.reservationRepo.GetReservationByID(request.ReservationID)
 	if err != nil {
 			return nil, fmt.Errorf("error fetching reservation: %w", err)
+	}
+	
+	bookStock, err := lu.bookStockRepo.GetBookStockByID(request.BookStockID)
+	if err != nil {
+			return nil, fmt.Errorf("error fetching book stock: %w", err)
 	}
 
 		if reservation.Status != "pending" {
 			return nil, fmt.Errorf("reservation is not pending")
-	}*/
+	}
+	
+	if bookStock.Status != "available" {
+			return nil, fmt.Errorf("book stock is not available")
+	}
 
 	err := lu.reservationRepo.UpdateReservationStatus(reservationID, "collected")
 	if err != nil {
 		return nil, fmt.Errorf("failed to update reservation status: %w", err)
 	}
 
-	/*bookStock, err := lu.bookStockRepo.GetBookStockByID(request.BookStockID)
-	if err != nil {
-			return nil, fmt.Errorf("error fetching book stock: %w", err)
-	}
-
-	// Ensure book stock status is "available"
-	if bookStock.Status != "available" {
-			return nil, fmt.Errorf("book stock is not available")
-	}*/
-
 	err = lu.bookStockRepo.UpdateStockStatus(request.BookStockID, "borrowed")
 	if err != nil {
 		return nil, fmt.Errorf("failed to update book stock status: %w", err)
 	}
-	// Calculate return_by using reservation's BorrowedDays and current timestamp
-	/*returnBy := time.Now().AddDate(0, 0, reservation.BorrowedDays)
+	
+	returnBy := time.Now().AddDate(0, 0, reservation.BorrowedDays)
 
 	loan := &model.Loan{
 			ReturnBy:      returnBy,
 			BookStockID:   request.BookStockID,
 			ReservationID: request.ReservationID,
-	}*/
+	}
 
 	// Create loan
 	createdLoan, err := lu.loanRepo.CreateLoan(loan)
