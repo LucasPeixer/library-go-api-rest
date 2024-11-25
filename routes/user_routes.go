410f83a8d3cfc17074593c1f44b40b55299eeaac
PoweredTable <poweredlucas@gmail.com>
2024-11-25 10:36:19 -0300

    refactor: move /user/register route to /users/register

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -25,6 +25,7 @@ func UserRoutes(rg *gin.RouterGroup) {
 
 	users := rg.Group("/users", middleware.JWTAuthMiddleware, middleware.RoleRequired("admin"))
 	{
+		users.POST("/register", userController.Register)
 		users.GET("/", userController.GetUsersByFilters)
 		users.GET("/:id", userController.GetUserById)
 		users.PUT("/activate/:id", userController.ToggleUser("activate"))
@@ -41,11 +42,6 @@ func UserRoutes(rg *gin.RouterGroup) {
 	user := rg.Group("/user")
 	{
 		user.POST("/login", userController.Login)
-		user.POST("/register",
-			middleware.JWTAuthMiddleware,
-			middleware.RoleRequired("admin"),
-			userController.Register,
-		)
 
 		reservations := user.Group("/reservations", middleware.JWTAuthMiddleware)
 		{
