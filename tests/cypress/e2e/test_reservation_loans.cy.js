2f6d6d7c42e010b0e66668f9bfa4fc6127cbb6ce
WRbosss <waldyrprograming@gmail.com>
2024-11-25 22:34:52 -0300

    feat: add test routes for loan completion and book deletion

--- tests/cypress/e2e/test_reservation_loans.cy.js
+++ tests/cypress/e2e/test_reservation_loans.cy.js
@@ -9,7 +9,7 @@ describe('API tests', () => {
             method: 'POST',
             url: 'http://localhost:8080/api/v1/login',
             body: {
-                "email": "waldyrrcosta@gmail.com",
+                "email": "lucas@admin.com",
                 "password": "123"
             }
         }).then((response) => {
@@ -29,8 +29,8 @@ describe('API tests', () => {
                 Authorization: `Bearer ${authToken}`,
             },
             body: {
-                "title": "Casa amarela 9",
-                "synopsis": "Uma casa que um dia foi amarela 9",
+                "title": "Casa amarela 17",
+                "synopsis": "Uma casa que um dia foi amarela 17",
                 "author_id": 2,
                 "genre_ids": [
                     1, 2, 3, 4, 5
@@ -39,8 +39,8 @@ describe('API tests', () => {
         }).then((response) => {
 
             expect(response.status).to.equal(201)
-            expect(response.body).to.have.property('title', "Casa amarela 9")
-            expect(response.body).to.have.property('synopsis', "Uma casa que um dia foi amarela 9")
+            expect(response.body).to.have.property('title', "Casa amarela 17")
+            expect(response.body).to.have.property('synopsis', "Uma casa que um dia foi amarela 17")
             expect(response.body).to.have.property('amount', 0)
             expect(response.body.author).to.deep.equal(author);
             expect(response.body.genres).to.deep.equal(genres);
@@ -61,7 +61,7 @@ describe('API tests', () => {
                 Authorization: `Bearer ${authToken}`
             },
             body: {
-                "code": 107
+                "code": 117
             }
         }).then((response) => {
             expect(response.status).to.eq(200);
@@ -128,7 +128,40 @@ describe('API tests', () => {
         }).then((response) => {
             expect(response.status).to.eq(201)
             const objectResponse = response.body
+            const loanId = response.body.id
             Cypress.env('responseLoan', objectResponse);
+            Cypress.env('loanId', loanId);
+        });
+    });
+    it('Update finished loan', () => {
+        const loanId = Cypress.env('loanId');
+
+        cy.wait(2000)
+        cy.api({
+            method: 'PUT',
+            url: `http://localhost:8080/api/v1/loans/finish-loan/${loanId}`,
+            headers: {
+                Authorization: `Bearer ${authToken}`
+            },
+            body: {
+                "status":"returned"
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body).to.have.property('message', "loan updated successfully")
+        });
+    });
+    it('Delete book', () => {
+        const bookId = Cypress.env('bookId');
+        cy.api({
+            method: 'DELETE',
+            url: `http://localhost:8080/api/v1/books/delete/${bookId}`,
+            headers: {
+                Authorization: `Bearer ${authToken}`
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body).to.have.property('message', "Book deleted successfully")
         });
     });
 
